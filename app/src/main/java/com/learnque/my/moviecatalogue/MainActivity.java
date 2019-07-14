b79e0f4adfb60ff54be61b61e4922a549ab67415
Habibul Umam <oemampedia@gmail.com>
2019-07-14 17:23:49 +0700

    Tambah button di  halaman utama selesai

--- app/src/main/java/com/learnque/my/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/learnque/my/moviecatalogue/MainActivity.java
@@ -1,10 +1,13 @@
 package com.learnque.my.moviecatalogue;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,4 +37,18 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_fav_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add_fav) {
+            Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }

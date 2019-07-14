dba9af392902e2fb72b411ccb45947a3a845b944
Habibul Umam <oemampedia@gmail.com>
2019-07-14 17:13:52 +0700

    Tampilan halaman utama selesai

--- app/src/main/java/com/learnque/my/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/learnque/my/moviecatalogue/MainActivity.java
@@ -1,13 +1,37 @@
 package com.learnque.my.moviecatalogue;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MovieFragment(), "MOVIE");
+        adapter.addFragment(new TVShowFragment(), "TV SHOW");
+        viewPager.setAdapter(adapter);
+    }
+
 }
